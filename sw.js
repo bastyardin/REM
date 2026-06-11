2eb2dc995eb5a4d2a06781d2e52026ca9f83f2b8
bastyardin <bostjancebron76@gmail.com>
2026-06-11 22:22:10 +0200

    bugs

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Ob spremembi aplikacije povečajte številko VERZIJA spodaj,
    da brskalniki prenesejo svežo kopijo.
    ============================================================ */
-const VERZIJA = 'v6';
+const VERZIJA = 'v7';
 const PREDPOMNILNIK = 'upravljalec-nepremicnin-' + VERZIJA;
 
 /* datoteke, ki sestavljajo aplikacijo (app shell) */
