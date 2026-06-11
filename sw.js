9d616e06b12f37a3a81c5c1cb0615ba65304ac77
bastyardin <bostjancebron76@gmail.com>
2026-06-11 22:36:48 +0200

    majhne izboljšave + skaliranje

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Ob spremembi aplikacije povečajte številko VERZIJA spodaj,
    da brskalniki prenesejo svežo kopijo.
    ============================================================ */
-const VERZIJA = 'v7';
+const VERZIJA = 'v8';
 const PREDPOMNILNIK = 'upravljalec-nepremicnin-' + VERZIJA;
 
 /* datoteke, ki sestavljajo aplikacijo (app shell) */
